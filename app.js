9dda8f95f9f392c7789bfb17ee937384a7160002
Gleb Obitotsky <pluginexteragram@gmail.com>
2026-08-17 14:37:47 +1000

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const CONFIG = {
   source: {
     owner: "ligroku",
     repo: "mihous",
-    branch: "master",
+    branch: "main",
     github: "https://github.com/ligroku/mihous"
   }
 };
@@ -36,7 +36,7 @@ const CONFIG = {
   source: {
     owner: "ligroku",
     repo: "mihous",
-    branch: "master",
+    branch: "main",
     github: "https://github.com/ligroku/mihous"
   }
 };
